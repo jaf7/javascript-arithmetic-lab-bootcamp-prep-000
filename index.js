866270c3c9ab9bd9d07911356809c83bc49fb70d
Anthony Fields <janthonyfields@gmail.com>
2017-09-08 23:16:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -29,3 +29,11 @@ function makeInt( n ) {
     return parseInt( n, 10 );
   }
 }
+
+function preserveDecimal( n ) {
+  if ( isNaN(n) === true ) {
+    return NaN;
+  } else {
+    return parseFloat( n );
+  }
+}
